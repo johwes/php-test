1782b48b1b39b18f5bf3168a8e360c150bf5a760
root <root@master.example.com>
2016-05-14 19:42:33 +0200

    Add no cache + title

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<head>
+<title>
+TEST
+</title>
+<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+<meta http-equiv="Pragma" content="no-cache" />
+<meta http-equiv="Expires" content="0" />
+</head>
 <html> 
 <body style="background-color: white">
 <p>
