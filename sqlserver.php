5a0b30b3ae2c809be6b8db80a305a421d7c83529
johwes <johnny.westerlund@gmail.com>
2022-07-05 15:37:46 +0200

    Delete sqlserver.php

--- sqlserver.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host ="sqlserver"; 
-$username ="sa";
-$password ="yourStrong(!)Password";
-$database ="test";
-
-mssql_connect($host, $username, $password);
-mssql_select_db($database);
-?>
